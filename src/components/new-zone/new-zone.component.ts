843a26c7042902d20b208e315a0da266de30d877
Nelli_Hayrapetyan <nhayrapetyan@slb.com>
2023-07-19 22:33:00 +0400

    draw path after mouse click

--- src/components/new-zone/new-zone.component.ts
+++ src/components/new-zone/new-zone.component.ts
@@ -9,14 +9,24 @@ export class NewZoneComponent {
   @Input() zone?: Zone;
   @Output() createPolygon = new EventEmitter<[number, number][]>();
   pointCount = 0;
-
+  isDrawing = false;
   points: [number, number][] = [];
+  mainPoints: [number, number][] = [];
 
   drawPolygon(event: MouseEvent): void {
+    this.isDrawing = true;
     this.points = [...this.points, [ event.offsetX, event.offsetY ]];
     this.pointCount += 1;
+    this.mainPoints = this.points;
     if (this.pointCount === 4) {
       this.createPolygon.emit(this.points);
+      this.isDrawing = false;
+    }
+  }
+
+  moveZoneLine(event: any) {
+    if (this.isDrawing) {
+      this.points = [...this.mainPoints, [ event.offsetX, event.offsetY ]]
     }
   }
 }
